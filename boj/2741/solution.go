eb8e99f86a499641c7cc65876b58b8b527648559
minseok-oh <minseok.dev@gmail.com>
2026-01-23 12:25:27 +0900

    solve: boj 2741

---
+++ boj/2741/solution.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var n int
+	if _, err := fmt.Fscan(in, &n); err != nil {
+		return
+	}
+
+	for i := 1; i <= n; i++ {
+		fmt.Fprintln(out, i)
+	}
+}
