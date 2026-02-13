493a6d20fad3dd24c39687be186a009d3623204a
sinsohi <sohi8451@naver.com>
2026-02-13 16:14:56 +0900

    solve: boj 11650

---
+++ boj/p11650/solution.java
@@ -0,0 +1,47 @@
+package boj.p11650;
+
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    static int n;
+    static int[][] dots;
+
+    public static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        dots = new int[n][2];
+
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            dots[i][0] = Integer.parseInt(st.nextToken());
+            dots[i][1] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    public static void sort() {
+        Arrays.sort(dots, (dot1, dot2) -> {
+            if (dot1[0] == dot2[0]) {
+                return Integer.compare(dot1[1], dot2[1]);
+            } else {
+                return Integer.compare(dot1[0], dot2[0]);
+            }
+        });
+    }
+
+    public static void print() {
+        StringBuilder output = new StringBuilder();
+        for (int[] dot : dots) {
+            output.append(dot[0]).append(" ").append(dot[1]).append("\n");
+        }
+        System.out.print(output.toString());
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        sort();
+        print();
+    }
+}
